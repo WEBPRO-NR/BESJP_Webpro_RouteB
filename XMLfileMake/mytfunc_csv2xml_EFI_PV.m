b071e09b9cb0b1c68a31c397247fe3095a8b8b08
Masato Miyata <makun@myts.jp>
2017-04-14 08:30:35 +0900

    太陽光発電：Ver2の入力シートに対応

--- XMLfileMake/mytfunc_csv2xml_EFI_PV.m
+++ XMLfileMake/mytfunc_csv2xml_EFI_PV.m
@@ -51,7 +51,7 @@ PV_Info = {};
 for iUNIT = 11:size(PVDataCell,1)
     
     % 名称と発電効率が空欄の場合はスキップ
-    if isempty(PVDataCell{iUNIT,1}) == 0 && isempty(PVDataCell{iUNIT,4}) == 0
+    if isempty(PVDataCell{iUNIT,1}) == 0 && isempty(PVDataCell{iUNIT,5}) == 0
         
         % 太陽光発電システム名称
         if isempty(PVDataCell{iUNIT,1})
@@ -61,10 +61,10 @@ for iUNIT = 11:size(PVDataCell,1)
         end
         
         % 太陽電池の種類
-        if isempty(PVDataCell{iUNIT,2}) == 0
-            if strcmp(PVDataCell(iUNIT,2),'結晶系')
+        if isempty(PVDataCell{iUNIT,3}) == 0
+            if strcmp(PVDataCell(iUNIT,3),'結晶系')
                 PV_Type = [PV_Type;'Crystalline'];
-            elseif strcmp(PVDataCell(iUNIT,2),'結晶系以外')
+            elseif strcmp(PVDataCell(iUNIT,3),'結晶系以外')
                 PV_Type = [PV_Type;'NonCrystalline'];
             else
                 error('太陽光発電: 太陽電池の種類の選択肢が不正です')
@@ -74,12 +74,12 @@ for iUNIT = 11:size(PVDataCell,1)
         end
         
         % アレイ設置方式
-        if isempty(PVDataCell{iUNIT,3}) == 0
-            if strcmp(PVDataCell(iUNIT,3),'架台設置形')
+        if isempty(PVDataCell{iUNIT,4}) == 0
+            if strcmp(PVDataCell(iUNIT,4),'架台設置形')
                 PV_InstallationMode = [PV_InstallationMode;'RackMountType'];
-            elseif strcmp(PVDataCell(iUNIT,3),'屋根置き形')
+            elseif strcmp(PVDataCell(iUNIT,4),'屋根置き形')
                 PV_InstallationMode = [PV_InstallationMode;'RoomMountType'];
-            elseif strcmp(PVDataCell(iUNIT,3),'その他')
+            elseif strcmp(PVDataCell(iUNIT,4),'その他')
                 PV_InstallationMode = [PV_InstallationMode;'Others'];
             else
                 error('太陽光発電: アレイ設置方式の選択肢が不正です')
@@ -89,37 +89,37 @@ for iUNIT = 11:size(PVDataCell,1)
         end
         
         % アレイのシステム容量
-        if isempty(PVDataCell{iUNIT,4}) == 0
-            PV_Capacity = [PV_Capacity; PVDataCell{iUNIT,4}];
+        if isempty(PVDataCell{iUNIT,5}) == 0
+            PV_Capacity = [PV_Capacity; PVDataCell{iUNIT,5}];
         else
             error('太陽光発電：アレイのシステム容量が空欄です')
         end
         
         % パネルの方位角
-        if isempty(PVDataCell{iUNIT,5}) == 0
-            PV_PanelDirection = [PV_PanelDirection; PVDataCell{iUNIT,5}];
+        if isempty(PVDataCell{iUNIT,6}) == 0
+            PV_PanelDirection = [PV_PanelDirection; PVDataCell{iUNIT,6}];
         else
             error('太陽光発電：パネルの方位角が空欄です')
         end
         
         % パネルの傾斜角
-        if isempty(PVDataCell{iUNIT,6}) == 0
-            PV_PanelAngle = [PV_PanelAngle; PVDataCell{iUNIT,6}];
+        if isempty(PVDataCell{iUNIT,7}) == 0
+            PV_PanelAngle = [PV_PanelAngle; PVDataCell{iUNIT,7}];
         else
             error('太陽光発電：パネルの傾斜角が空欄です')
         end
         
         % 年間日射量地域区分
-        if isempty(PVDataCell{iUNIT,7}) == 0
-            if strcmp(PVDataCell(iUNIT,7),'A地域')
+        if isempty(PVDataCell{iUNIT,8}) == 0
+            if strcmp(PVDataCell(iUNIT,8),'A地域')
                 PV_SolorIrradiationRegion = [PV_SolorIrradiationRegion;'A'];
-            elseif strcmp(PVDataCell(iUNIT,7),'B地域')
+            elseif strcmp(PVDataCell(iUNIT,8),'B地域')
                 PV_SolorIrradiationRegion = [PV_SolorIrradiationRegion;'B'];
-            elseif strcmp(PVDataCell(iUNIT,7),'C地域')
+            elseif strcmp(PVDataCell(iUNIT,8),'C地域')
                 PV_SolorIrradiationRegion = [PV_SolorIrradiationRegion;'C'];
-            elseif strcmp(PVDataCell(iUNIT,7),'D地域')
+            elseif strcmp(PVDataCell(iUNIT,8),'D地域')
                 PV_SolorIrradiationRegion = [PV_SolorIrradiationRegion;'D'];
-            elseif strcmp(PVDataCell(iUNIT,7),'E地域')
+            elseif strcmp(PVDataCell(iUNIT,8),'E地域')
                 PV_SolorIrradiationRegion = [PV_SolorIrradiationRegion;'E'];
             else
                 error('太陽光発電: 年間日射量地域区分の選択肢が不正です')
@@ -129,10 +129,10 @@ for iUNIT = 11:size(PVDataCell,1)
         end
         
         % 備考
-        if isempty(PVDataCell{iUNIT,8})
+        if isempty(PVDataCell{iUNIT,9})
             PV_Info   = [PV_Info;'Null'];
         else
-            PV_Info   = [PV_Info;PVDataCell{iUNIT,8}];
+            PV_Info   = [PV_Info;PVDataCell{iUNIT,9}];
         end
         
     end
